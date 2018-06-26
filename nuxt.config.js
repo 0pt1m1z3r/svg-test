382cd5f90b8ed4019dacea3de9c764a367237875
Dmitri Efimenko <ezheg89@gmail.com>
2018-06-26 11:18:31 +0300

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,124 @@
+const path = require('path')
+
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '{{ name }}',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{escape description }}' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    extend (config, ctx) {
+      config.module.rules.forEach((rule) => {
+        // images
+        if (~rule.test.source.indexOf('|svg')) {
+          rule.test = new RegExp(rule.test.source.replace('|svg', ''))
+        }
+
+        // transform urls to require calls
+        if (rule.loader === 'vue-loader') {
+          rule.options.transformToRequire = {
+            'img': 'src',
+            'image': 'xlink:href',
+            'use': 'xlink:href'
+          }
+          return
+        }
+      })
+
+      // svg
+      config.module.rules.push({
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          symbolId: this.options.dev
+            ? '[1].[ext]'
+            : (new IncrementalCache()).get,
+          symbolRegExp: this.options.srcDir + '/(.*)\\.svg',
+          esModule: false,
+          extract: true,
+          spriteFilename: this.options.dev
+            ? spriteFilenameGeneratorDev.bind(this)()
+            : spriteFilenameGeneratorProd.bind(this)()
+        }
+      })
+    },
+    plugins: [
+      new SpriteLoaderPlugin()
+    ]
+  }
+}
+
+function spriteFilenameGeneratorDev() {
+  return (svgPath) => {
+    const pathKey = getSvgSpritePathKey({ svgPath, options: this.options })
+    const normalizedPathKey = normalizeSvgSpritePathKey(pathKey)
+    return `sprites/${normalizedPathKey}.svg`
+  }
+}
+
+function spriteFilenameGeneratorProd(svgPath) {
+  const cache = new IncrementalCache()
+  cache.get('default') // set 0 for default
+  return (svgPath) => {
+    const pathKey = getSvgSpritePathKey({ svgPath, options: this.options })
+    const normalizedPathKey = normalizeSvgSpritePathKey(pathKey)
+    const id = cache.get(normalizedPathKey)
+    return `sprites/${id}.[hash:7].svg`
+  }
+}
+
+function normalizeSvgSpritePathKey(pathKey) {
+  return pathKey.toLowerCase().replace(/\//g, '-')
+}
+
+function getSvgSpritePathKey({ svgPath, options }) {
+  if (~svgPath.indexOf(options.srcDir)) {
+    const relPath = svgPath.slice(options.srcDir.length + 1)
+    const rules = [
+      // components first level
+      /(components[\/\\][^\/\\]+)[\/\\]/
+    ]
+    let m
+    for (rule of rules) {
+      m = relPath.match(rule)
+      if (m && m[1]) return m[1]
+    }
+    // pages
+    for (route of options.router.routes) {
+      if (~relPath.indexOf(options.dir.pages + route.path)) {
+        return options.dir.pages + route.path
+      }
+    }
+  }
+  return 'default'
+}
+
+function IncrementalCache() {
+  this.store = {}
+  this.lastId = 0
+
+  this.get = (key) => {
+    if (!this.store[key]) {
+      this.store[key] = (this.lastId++).toString(36)
+    }
+    return this.store[key]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <div>
+    <h3>&lt;svg&gt;&lt;use&gt;</h3>
+    <svg style="width: 200px; height: 200px;">
+      <use xlink:href="./images/bestdeals.svg"></use>
+    </svg>
+    <svg style="width: 200px; height: 200px;">
+      <use xlink:href="./images/profile.svg"></use>
+    </svg>
+    <svg style="width: 200px; height: 200px;">
+      <use xlink:href="./images/select.svg"></use>
+    </svg>
+  </div>
+  <div>
+    <h3>&lt;img&gt;</h3>
+    <img style="width: 200px; height: 200px;" src="./images/bestdeals.svg">
+    <img style="width: 200px; height: 200px;" src="./images/profile.svg">
+    <img style="width: 200px; height: 200px;" src="./images/select.svg">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
